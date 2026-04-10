0de8b1b1a008113b80bcdb4f757d4fb51806ea0f
junki <gofish948@icloud.com>
2026-04-10 20:09:36 +0900

    안드로이드 빌드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // '파란색'이 아니라 '주요 색상'이라는 역할을 부여합니다.
+        primary: {
+          DEFAULT: '#2563EB',
+        },
+        // 에러, 성공 등의 상태 색상도 미리 정의합니다.
+        danger: '#EF4444',
+        success: '#10B981',
+      }
+    },
   },
   plugins: [],
 };
